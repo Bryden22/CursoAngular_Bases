f7e0f629b7542fa439321be1c0396ac51c1ed1af
Juan Gutierrez <jgutierrez@alu.ing.unlp.edu.ar>
2021-07-08 17:47:28 -0300

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadoresModule } from './Contadores/contadores.module';
+
+import { HeroesModule } from './Heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadoresModule
   ],
   providers: [],
   bootstrap: [AppComponent]
